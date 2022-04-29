0a0b7d0e5d3e0d9c1ee33ba8118d0b37a0e362b9
navya <nallamothunavya02@gmail.com>
2022-04-29 10:22:38 +0530

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //   throw new Error("Incorrect input!");
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return result;
+    }
+}
+var number1;
+number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
+console.log(result);
